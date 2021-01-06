dc61e4268ce04bfe0ae82a4d50829b1dd398482a
jakub-coding <ljakubsky@jakub-coding.de>
2021-01-06 14:21:33 +0100

    base app with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import { NavLink} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App container mx-auto">
+      <header className="App-header py-16">
+        <nav className="flex justify-center items-center">
+          <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/tunes">Tunes</NavLink>
+          <NavLink className="nav-link" to="/about">About</NavLink>
+        </nav>
       </header>
+
+      <main>
+        <img src={logo} className="App-logo" alt="logo" />
+      </main>
     </div>
   );
 }
